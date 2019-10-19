59ccf734eeaec7fa32c5713f5002eab9e113977a
Adriaz <53216669+Adriaz@users.noreply.github.com>
2019-10-19 16:17:09 +0800

    Major Migration
    Experimental

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=eibconcierge2-Web-Deploy.pubxml -p:Password=D66jmJg1nYKPs8EobXlrKJi8Zyi9L7kT8dr34CLkHhn6u7L7KqJkdmnYLm8e
+
